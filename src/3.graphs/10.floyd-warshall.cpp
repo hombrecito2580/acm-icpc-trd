75ebf2ee9a9567ab6769852403dee26f0913fb80
Pratyush Kumar Chaturvedi <hombrecito2580@gmail.com>
2024-12-28 10:11:38 +0530

    Floyd warshall

---
+++ src/3.graphs/10.floyd-warshall.cpp
@@ -0,0 +1,15 @@
+vector<vector<int>> floydWarshall(vector<vector<int>>& graph, int V) {
+    vector<vector<int>> dist = graph;
+
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+    return dist;
+}
